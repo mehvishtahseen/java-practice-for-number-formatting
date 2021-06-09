711838a4075ca0091dc4601d7663ee1177c6138a
mehvishtahseen <54951320+mehvishtahseen@users.noreply.github.com>
2021-06-09 12:54:21 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+       //Formatting numbers
+        // use of formatting currency class to convert a input string to currency when needed
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String returnCurrency = currency.format(123456789.98765);
+        System.out.println(returnCurrency);
+
+        //Using the number formatting class to print the given input as a percentage
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String returnPercentage = percent.format(0.90);
+        System.out.println(returnPercentage);
+
+        //printing the percentage using the method chaining process where two methods are combined
+        String returnPercent = NumberFormat.getPercentInstance().format(0.890);
+        System.out.println(returnPercent);
     }
 }
